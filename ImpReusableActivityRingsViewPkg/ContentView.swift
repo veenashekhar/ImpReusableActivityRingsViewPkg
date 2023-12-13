0692824f2d104721e06111183c7b0ec19645c276
Veena <veenashkr1@gmail.com>
2023-12-13 16:13:38 +0530

    Added details of definitions

--- ImpReusableActivityRingsViewPkg/ContentView.swift
+++ ImpReusableActivityRingsViewPkg/ContentView.swift
@@ -7,52 +7,47 @@
 
 import SwiftUI
 import SwiftData
+import ReusableActivityRIngsViewPkg
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    
+    //Defining Array with progress to be shown for each ring
+    // Supports upto 7 rings
+    @State private var progressArray: [CGFloat] = [1.0,1.0,0.75]
+    
+    
+    /* Color Array
+     1 = Red
+     2 = Blue
+     3 = Green
+     4 = Magenta
+     5 = Orange
+     6 = Mint
+     7 = Crimson
+     */
+     
+    //Defining color for each ring of progress
+    @State private var colorArray: [Int] = [3,6,7]
+
+/*
+  If number of elements in progressArray equals the number of elements in colorArray, the rings will be displayed with respective colors.
+ Otherwise, default values shown above will be used.
+ Ex: progressArray = [0.2, 0.5,0.6,0.4]
+     colorArray = [] or colorArray = [1]
+     will generate 4 progress rings with default colors - Red, Blue, Green and Magenta
+ 
+*/
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
+        ZStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            ActivityRingsView(progArr: $progressArray, colorArr: $colorArray).fixedSize()
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
         }
-    }
+             }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+    
 }
 
 #Preview {
